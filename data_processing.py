9a60be75114dd0069e8aedc320c830382d8e0ac8
shaleen25bai10034-creator <shaleen.25bai10034@vitbhopal.ac.in>
2026-03-30 12:33:47 +0530

    Delete data_processing.py

--- data_processing.py
+++
@@ -1,9 +0,0 @@
-data = []
-
-def add_data():
-    subject = input("Subject: ")
-    hours = float(input("Hours: "))
-    data.append((subject, hours))
-
-def get_total():
-    return sum(h for s, h in data)
